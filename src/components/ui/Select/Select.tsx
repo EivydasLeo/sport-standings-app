25eb2c4f227c03fcc92d71296982f95cf4c90662
EivydasLeo <eivydasleo@gmail.com>
2025-08-07 19:16:26 +0300

    chore: Updated Select component.

--- src/components/ui/Select/Select.tsx
+++ src/components/ui/Select/Select.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import styles from "./Select.module.scss";
 import { type SelectProps } from "./Select.types";
 
-export const Select: React.FC<SelectProps> = ({ options, className, ...props }) => {
+export const Select: React.FC<SelectProps> = ({ options, className, name, ...props }) => {
     return (
-        <select className={`${styles.select} ${className || ""}`} {...props}>
+        <select
+            className={`${styles.select} ${className || ""}`}
+            name={name}
+            defaultValue=""
+            {...props}
+        >
+            <option value="" disabled>
+                {name === "home" ? "Home Team" : "Away Team"}
+            </option>
             {options.map((option) => (
                 <option key={option.value} value={option.value}>
                     {option.label}
