789b86f1f35149d12c5cbdf692e20327cbdcc379
EivydasLeo <eivydasleo@gmail.com>
2025-08-07 19:14:26 +0300

    chore: Created data, style changes.

---
+++ src/data/basketball.ts
@@ -0,0 +1,36 @@
+import type { TableRow } from "../components/ui/Table/Table.types";
+
+export const basketballStandings: TableRow[] = [
+    {
+        name: "France",
+        wins: 0,
+        draws: 1,
+        losses: 0,
+        points: 1,
+        flag: "/flags/france.svg",
+    },
+    {
+        name: "Lithuania",
+        wins: 1,
+        draws: 0,
+        losses: 0,
+        points: 3,
+        flag: "/flags/lithuania.svg",
+    },
+    {
+        name: "Spain",
+        wins: 0,
+        draws: 1,
+        losses: 0,
+        points: 0,
+        flag: "/flags/spain.svg",
+    },
+    {
+        name: "Germany",
+        wins: 0,
+        draws: 1,
+        losses: 0,
+        points: 1,
+        flag: "/flags/germany.svg",
+    },
+];
